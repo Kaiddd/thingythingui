0e799e07562b69675b794556dc4fcbe3ec877779
Kaede <37759450+Kaiddd@users.noreply.github.com>
2026-02-03 13:13:57 +0000

    \n

--- m.lua
+++ m.lua
@@ -1,7 +1,7 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Kaiddd/thingythingui/refs/heads/main/ui2.lua"))()
 Library:Notify({
     Title = "Trial Ended",
-    Description = "Your Yabujin free trial has ended! If you would like to continue using our product, we are temporarily offering a discount to all of our users who cannot afford it, purchase at https://yabu.mysellauth.com/product/yabujin-keys to save 10% on your yabujin license ❤️, link was copied to your clipboard!",
+    Description = "Your Yabujin free trial has ended! If you would like to continue using our product, we are temporarily offering a discount to all of our users who cannot afford it.\nPurchase at https://yabu.mysellauth.com/product/yabujin-keys to save 10% on your yabujin license ❤️, link was copied to your clipboard!",
     Duration = 150,
 })
 setclipboard('https://yabu.mysellauth.com/product/yabujin-keys')
