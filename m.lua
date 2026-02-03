2f9471544ba813f21675e9fe3d88d6ae7d81e650
Kaede <37759450+Kaiddd@users.noreply.github.com>
2026-02-03 13:04:44 +0000

    time

--- m.lua
+++ m.lua
@@ -2,6 +2,6 @@ local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Kaidd
 Library:Notify({
     Title = "Trial Ended",
     Description = "Your Yabujin free trial has ended! If you would like to continue using our product, we are temporarily offering a discount to all of our users who cannot afford it, purchase at https://yabu.mysellauth.com/product/yabujin-keys to save 10% on your yabujin license ❤️, link was copied to your clipboard!",
-    Duration = 15,
+    Duration = 150,
 })
 setclipboard('https://yabu.mysellauth.com/product/yabujin-keys')
